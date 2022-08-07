ae787695e61085b1ad17949b0e28a03f69ce2e65
Abhishek <abhishek0058.be21@chitkara.edu.in>
2022-08-07 23:45:48 +0530

    font links added

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //https://api.openweathermap.org/data/2.5/weather?q=Pune&appid=65640da40dcba6b9f1fdcf677c3b2d91
+
+const curDate = document.getElementById("date");
+let weathericon = document.getElementById("weathericon");
+const tempstatus = "Clouds";
